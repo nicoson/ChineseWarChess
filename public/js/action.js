35d0a79f9dccd5f41273fc55af25d5ec6b64861a
nicoson <nicoson@163.com>
2015-03-14 10:38:04 -0400

    version 1.0.0

--- public/js/action.js
+++ public/js/action.js
@@ -152,6 +152,45 @@ function xiangAction(x,y,matrix){
 	return availLattice;
 }
 
+function xiangUpAction(x,y,matrix){
+	var availLattice = [[y,x]];
+	
+	if(y-2>0){	//upper
+		if(x-2>-1){	//up-left
+			if(matrix[y-1][x-1]==0 || matrix[y-1][x-1]==2){
+				if(matrix[y-2][x-2]!=1){
+					availLattice.push([y-2,x-2]);
+				}
+			}
+		}
+		if(x+2<9){ //up-right
+			if(matrix[y-1][x+1]==0 || matrix[y-1][x+1]==2){
+				if(matrix[y-2][x+2]!=1){
+					availLattice.push([y-2,x+2]);
+				}
+			}
+		}
+	}
+	if(y+2<5){
+		if(x-2>-1){	//down-left
+			if(matrix[y+1][x-1]==0 || matrix[y+1][x-1]==2){
+				if(matrix[y+2][x-2]!=1){
+					availLattice.push([y+2,x-2]);
+				}
+			}
+		}
+		if(x+2<9){ //down-right
+			if(matrix[y+1][x+1]==0 || matrix[y+1][x+1]==2){
+				if(matrix[y+2][x+2]!=1){
+					availLattice.push([y+2,x+2]);
+				}
+			}
+		}
+	}
+
+	return availLattice;
+}
+
 function shiAction(x,y,matrix){
 	var availLattice = [[y,x]];
 
@@ -182,6 +221,36 @@ function shiAction(x,y,matrix){
 	return availLattice;
 }
 
+function shiUpAction(x,y,matrix){
+	var availLattice = [[y,x]];
+
+	if(y>0){	//up
+		if(x-1>2){	// up-left
+			if(matrix[y-1][x-1]==0 || matrix[y-1][x-1]==2){
+				availLattice.push([y-1,x-1]);
+			}
+		}
+		if(x+1<6){	// up-right
+			if(matrix[y-1][x+1]==0 || matrix[y-1][x+1]==2){
+				availLattice.push([y-1,x+1]);
+			}
+		}
+	}
+	if(y+1<3){	//down
+		if(x-1>2){	// down-left
+			if(matrix[y+1][x-1]==0 || matrix[y+1][x-1]==2){
+				availLattice.push([y+1,x-1]);
+			}
+		}
+		if(x+1<6){	// down-left
+			if(matrix[y+1][x+1]==0 || matrix[y+1][x+1]==2){
+				availLattice.push([y+1,x+1]);
+			}
+		}
+	}
+	return availLattice;
+}
+
 function shuaiAction(x,y,matrix){
 	var availLattice = [[y,x]];
 	if(y-1>6){	// up
@@ -191,6 +260,35 @@ function shuaiAction(x,y,matrix){
 	}
 
 	if(y+1<10){	// down
+		if(matrix[y+1][x]==0 || matrix[y+1][x]==2){
+			availLattice.push([y+1,x]);
+		}
+	}
+	
+	if(x-1>2){	// left
+		if(matrix[y][x-1]==0 || matrix[y][x-1]==2){
+			availLattice.push([y,x-1]);
+		}
+	}
+
+	if(x+1<6){	// right
+		if(matrix[y][x+1]==0 || matrix[y][x+1]==2){
+			availLattice.push([y,x+1]);
+		}
+	}
+	
+	return availLattice;
+}
+
+function shuaiUpAction(x,y,matrix){
+	var availLattice = [[y,x]];
+	if(y>0){	// up
+		if(matrix[y-1][x]==0 || matrix[y-1][x]==2){
+			availLattice.push([y-1,x]);
+		}
+	}
+
+	if(y+1<3){	// down
 		if(matrix[y+1][x]==0 || matrix[y+1][x]==2){
 			availLattice.push([y+1,x]);
 		}
@@ -279,6 +377,29 @@ function bingAction(x,y,matrix){
 	}
 
 	if(y<5){
+		if(x>0){	//left
+			if(matrix[y][x-1]==0 || matrix[y][x-1]==2){
+				availLattice.push([y,x-1]);
+			}
+		}
+		if(x<8){	//right
+			if(matrix[y][x+1]==0 || matrix[y][x+1]==2){
+				availLattice.push([y,x+1]);
+			}
+		}
+	}
+
+	return availLattice;
+}
+
+function bingUpAction(x,y,matrix){
+	var availLattice = [[y,x]];
+
+	if(matrix[y+1][x]==0 || matrix[y+1][x]==2){	// down
+			availLattice.push([y+1,x]);
+	}
+
+	if(y>4){
 		if(x>0){	//left
 			if(matrix[y][x-1]==0 || matrix[y][x-1]==2){
 				availLattice.push([y,x-1]);
